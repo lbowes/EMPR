d14cf50750ab6edcfb0e15e1b640fa47eaba6317
A Poptani <asim@poptani.com>
2019-10-18 12:21:53 +0100

    Added info

--- common_utils/LCDDisplay.c
+++ common_utils/LCDDisplay.c
@@ -5,13 +5,23 @@ static uint32_t I2CAddress = 0x38;
 
 // This LCD has 2X8 bit registers
 // Instruction reg and data Reg
+// Top 2 bytes have impact
+
+// First  bit  control byte 0 last control byte
+// 1 another control byte follows after data
+// Second bit
+// 0 instruction reg
+// 1 data reg
+// bits 5-0 is the data
 
 
 // DO NOT CHANGE this!!!!
+// Setup bold face
 static uint32_t initWrite[10] = {0x00, 0x34, 0x0c, 0x06, 0x35, 0x10, 0x42, 0x9f, 0x34, 0x02};
 
 void LCDDisplay_init(void) {
-    I2C_INIT(LPC_I2C0,)
+    //
+    I2C_INIT(LPC_I2C0,100000);
 // Clear Screen
 }
 
