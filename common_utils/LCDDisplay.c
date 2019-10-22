e77608ec51c967ed5833b571fc18445a3d875d2d
A Poptani <asim@poptani.com>
2019-10-22 15:13:01 +0100

    Added config

--- common_utils/LCDDisplay.c
+++ common_utils/LCDDisplay.c
@@ -1,6 +1,7 @@
 #include "LCDDisplay.h"
 #include <stdbool.h>
 #include <lpc17xx_i2c.h>
+#include <lpc17xx_pinsel.h>
 static uint32_t I2CAddress = 0x38;
 
 // This LCD has 2X8 bit registers
@@ -28,7 +29,21 @@ static uint32_t initWrite[10] = {0x00, 0x34, 0x0c, 0x06, 0x35, 0x10, 0x42, 0x9f,
 
 void LCDDisplay_init(void) {
     //
-    I2C_INIT(LPC_I2C0,100000);
+    I2C_INIT(LPC_I2C1,100000);
+    I2C_Cmd(LPC_I2C1,ENABLE);
+    I2C_M_SETUP_Type config ;
+//    = {I2CAddress, initWrite,10,0,NULL,0,0,2,0,0,0}
+    config.sl_addr7bit= I2CAddress;
+    config.tx_data= initWrite;
+    config.tx_length=10;
+    config.tx_count=0;
+    config.rx_data = NULL;
+    config.rx_length = 0;
+    config.rx_count = 0;
+
+    config.retransmissions_max = 2;
+
+    I2C_MasterTransferData(LPC_I2C1,&config,I2C_TRANSFER_POLLING);
 // Clear Screen
 }
 
