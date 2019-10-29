c1b8c3b3c62e08d033857db4eb017d12f39d5dbf
lbowes <lewisbowes0@gmail.com>
2019-10-29 14:17:49 +0000

    Add 'temp_keypad_testing' project to hold all code related to testing the keypad. This will be deleted once keypad library functions are complete.

--- common_utils/Delay.h
+++
@@ -1,4 +0,0 @@
-#ifndef EMPR_DELAY_H_
-#define EMPR_DELAY_H_
-void Delay_TenMS(void);
-#endif //EMPR_DELAY_H_
--- common_utils/Keypad.c
+++ common_utils/Keypad.c
@@ -10,5 +10,5 @@ bool keypad_isKeyDown(uint8_t empr_key)
     // This function takes in an integer representing a target key on the 4x4
     // keypad and returns whether or not this key is down.
 {
-
+    return false;
 }
