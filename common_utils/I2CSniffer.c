40767168a0b909ad2e9cdeb36fc25bbc73176605
lbowes <lewisbowes0@gmail.com>
2019-10-31 11:02:34 +0000

    Review modifications for Sniffer branch

--- common_utils/I2CSniffer.c
+++ common_utils/I2CSniffer.c
@@ -2,8 +2,22 @@
 
 #include <stdio.h>
 
-void sendData(I2C_M_SETUP_Type* setup,int address,uint8_t* Data, uint32_t dataLength)
-{
+
+static void I2CSniffer_setupPins(void) {
+    PINSEL_CFG_Type PinCFG;
+	PinCFG.Funcnum = 3;
+	PinCFG.OpenDrain = PINSEL_PINMODE_NORMAL;
+	PinCFG.Pinmode = PINSEL_PINMODE_PULLUP;
+
+	PinCFG.Portnum =0;
+	PinCFG.Pinnum = 0;
+	PINSEL_ConfigPin(&PinCFG);
+	PinCFG.Pinnum = 1;
+	PINSEL_ConfigPin(&PinCFG);
+}
+
+
+static void I2CSniffer_sendData(I2C_M_SETUP_Type* setup, int address, uint8_t* Data, uint32_t dataLength) {
 	setup->sl_addr7bit = address;
 
 	setup->tx_data = Data;
@@ -17,49 +31,29 @@ void sendData(I2C_M_SETUP_Type* setup,int address,uint8_t* Data, uint32_t dataLe
 	setup->retransmissions_max = 2;
 }
 
-void setupPins(void)
-{
-    // Setup for I2C pins
-    PINSEL_CFG_Type PinCFG;
-	PinCFG.Funcnum = 3;
-	PinCFG.OpenDrain = PINSEL_PINMODE_NORMAL;
-	PinCFG.Pinmode = PINSEL_PINMODE_PULLUP;
 
-	PinCFG.Portnum =0;
-	PinCFG.Pinnum = 0;
-	PINSEL_ConfigPin(&PinCFG);
-	PinCFG.Pinnum = 1;
-	PINSEL_ConfigPin(&PinCFG);
-}
-
-void sniffer(void) 
-{
+void I2CSniffer_run(void) {
     TextOutput_init();
-    TextOutput_print("Starting sniffer");
-    
+    TextOutput_print("Starting sniffer...");
+
     // Initialise I2C
-    I2C_Init(LPC_I2C1,100000);
-    I2C_Cmd(LPC_I2C1,ENABLE);
+    I2C_Init(LPC_I2C1, 100000);
+    I2C_Cmd(LPC_I2C1, ENABLE);
 
-    int index =0;
     setupPins();
-    TextOutput_print("Pins setup");
-    for (index; index<128; index ++)
-    {
-        uint8_t data[1]={0x00};
-        I2C_M_SETUP_Type setup ;
-        sendData(&setup,index,data,1);
-        Status a = I2C_MasterTransferData(LPC_I2C1,&setup,I2C_TRANSFER_POLLING);
-        if (a==SUCCESS)
-        {
-            char buff[100];
-            sprintf(buff,"Address is up: %d",index);
-            TextOutput_print(buff);
+    TextOutput_print("Completed pin setup.");
+
+    int queryAddress = 0;
+    for (queryAddress; queryAddress < 128; queryAddress++) {
+        I2C_M_SETUP_Type setup;
+        uint8_t data[1] = { 0x00 };
+        sendData(&setup, queryAddress, data, 1);
+
+        Status deviceDetected = I2C_MasterTransferData(LPC_I2C1, &setup, I2C_TRANSFER_POLLING);
+        if (deviceDetected) {
+            char notifcation[100];
+            sprintf(notifcation, "Device found at address %d", queryAddress);
+            TextOutput_print(notifcation);
         }
-
     }
-
-
 }
-
-
