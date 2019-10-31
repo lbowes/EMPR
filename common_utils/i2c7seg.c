72ec34b950c992e6d3916454f9c48ecbf378a82e
DJ Ramsden <djr543@cse168pc-03.cs.york.ac.uk>
2019-10-31 10:35:58 +0000

    Updated i2c7Seg

--- common_utils/i2c7seg.c
+++ common_utils/i2c7seg.c
@@ -1,58 +1,25 @@
 
 #include "i2c7seg.h"
-
+#include <common_utils/TextOutput.h>
 #include <LPC17xx.h>
 #include <lpc_types.h>
 #include <lpc17xx_i2c.h>
 #include <lpc17xx_pinsel.h>
+#include <common_utils/I2C.h>
 
 
 
-void init_i2c(void){
-	
-	PINSEL_CFG_Type pins;
-	
-	pins.Portnum = 0;
-	pins.OpenDrain = 0;
-	pins.Funcnum = 3;
-	pins.Pinmode = 0;
-	
-	pins.Pinnum = 9;
-	PINSEL_ConfigPin(&pins);
-	
-	pins.Pinnum = 10;
-	PINSEL_ConfigPin(&pins);
-	
-	
-	I2C_Init(LPC_I2C1, 100000);
-	
-	I2C_Cmd(LPC_I2C1, ENABLE);
 
+
+int send_data(uint8_t addr, uint8_t[7] Data){
+	
+	done = i2c_send_data(uint8_t addr,uint8_t* Data, uint32_t 7);
+	if (done == SUCCESS){
+		return 1;
 	}
-
-
-int send_data(uint8_t addr){
-	
-	I2C_M_SETUP_Type leds;
-	
-	leds.sl_addr7bit = addr;
-	leds.tx_data = &data;
-	leds.tx_length = sizeof(data);
-	leds.rx_length = 0;
-	leds.rx_data = NULL;
-	leds.retransmissions_max = 0x03;
-	
-	
-	I2C_MasterTransferData(LPC_I2C1, &leds, I2C_TRANSFER_POLLING); 
-	if (I2C_MasterTransferData(LPC_I2C1, &leds, I2C_TRANSFER_POLLING) == SUCCESS ){
-
-	return 1;
-		}
 	else{
-	return 0;
+		return 0;
 	}
-	
-
 
 }
 	
