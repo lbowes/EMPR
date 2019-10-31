0775891b949a41732e86c4f1c065cc57754047ff
DJ Ramsden <djr543@cse168pc-03.cs.york.ac.uk>
2019-10-31 11:20:13 +0000

    Asim & Dan - fixed I2C lib can init and send data WIP read data

--- common_utils/I2C.c
+++ common_utils/I2C.c
@@ -2,7 +2,11 @@
 
 // setup pins
 void i2c_init(void){
-    PINSEL_CFG_Type PinCFG;
+	// Initialise I2C
+    I2C_Init(LPC_I2C1, 100000);
+    I2C_Cmd(LPC_I2C1, ENABLE);
+    // Setup pins
+	PINSEL_CFG_Type PinCFG;
 	PinCFG.Funcnum = 3;
 	PinCFG.OpenDrain = PINSEL_PINMODE_NORMAL;
 	PinCFG.Pinmode = PINSEL_PINMODE_PULLUP;
@@ -16,12 +20,12 @@ void i2c_init(void){
 // Send data through I2C
 Status i2c_send_data(uint8_t address,uint8_t* Data, uint32_t dataLength)
 {
-    uint8_t data[1]={0x00};
+	// Setup data structure
     I2C_M_SETUP_Type setup;
-	setup.sl_addr7bit = 59;
+	setup.sl_addr7bit = address;
 
-	setup.tx_data = &data[0];
-	setup.tx_length = 1;
+	setup.tx_data = &Data[0];
+	setup.tx_length = dataLength;
 	setup.tx_count = 0;
 
 	setup.rx_data = NULL;
@@ -29,10 +33,7 @@ Status i2c_send_data(uint8_t address,uint8_t* Data, uint32_t dataLength)
 	setup.rx_count = 0;
 
 	setup.retransmissions_max = 2;
-    // TextOutput_print("Success");
-    Status a = I2C_MasterTransferData(LPC_I2C1,&setup,I2C_TRANSFER_POLLING);
-    if (a==SUCCESS){
-    TextOutput_print("Success");
-    }
+
+	// Return if SUCCESS =1 or ERROR=0
     return I2C_MasterTransferData(LPC_I2C1,&setup,I2C_TRANSFER_POLLING);
 }
