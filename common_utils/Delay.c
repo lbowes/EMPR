f68ffa778b413b5a45e72a5ec137c62e2e921e60
lbowes <lewisbowes0@gmail.com>
2019-11-05 10:35:22 +0000

    Replace hex with decimal in `Delay.c` for readability

--- common_utils/Delay.c
+++ common_utils/Delay.c
@@ -4,18 +4,18 @@
 #include "Delay.h"
 
 
-static uint32_t elapsed_Tick = 0x0;
+static uint32_t elapsed_Tick = 0;
 
 
 void SysTick_Handler(void) {
     elapsed_Tick++;
-    if ((elapsed_Tick % 0x02) == 0) {
+    if ((elapsed_Tick % 10) == 0) {
         Interrupt_tenMS();
     }
-    if ((elapsed_Tick % 0x07) == 0) {
+    if ((elapsed_Tick % 50) == 0) {
         Interrupt_fiftyMS();
     }
-    if ((elapsed_Tick % 0x14) == 0) {
+    if ((elapsed_Tick % 100) == 0) {
         Interrupt_hundredMS();
         elapsed_Tick = 0;
     }
