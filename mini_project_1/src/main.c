c3900dbd48453512162700ec52dca19e4ac6bdc3
lbowes <lewisbowes0@gmail.com>
2019-11-05 09:51:35 +0000

    Cleaned up `main.c` for `mini_project_1` (currently broken)

--- mini_project_1/src/main.c
+++ mini_project_1/src/main.c
@@ -6,42 +6,36 @@
 // • Print: “Finished count” on the terminal screen.
 #include <common_utils/LEDs.h>
 #include <common_utils/Delay.h>
-
 #include <common_utils/I2CSniffer.h>
-#include <stdint.h>
 #include <common_utils/TextOutput.h>
+
+#include <stdint.h>
 #include <stdio.h>
 
-int count = 0;
-int ledIndex = 0;
-char stringIndex[3];
+
+static int count = 0;
+static int ledIndex = 0;
+
 
 void Interrupt_tenMS(void) {
-    count++;
     if (count == 10) {
         count = 0;
+        char stringIndex[3];
         sprintf(stringIndex, "%d", ledIndex);
         TextOutput_print(stringIndex);
         LEDs_debugBinary(ledIndex++);
+
         if (ledIndex == 16) {
-            
             TextOutput_print("Count Finished");
             ledIndex = 0;
             Delay_Disable();
         }
     }
+
+    count++;
 }
 
 
 int main(void) {
-
-    I2CSniffer_run();
-    // LCDDisplay_init();
-    // Initialisating
-    // TextOutput_init();
-    // TextOutput_print("Starting count");
-    // LEDs_init();
-    // Delay_TenMS();
-    return 1;
-
+    return 0;
 }
