3d1c864c46655f1a4008a49911110a37a0b197ab
DJ Ramsden <djr543@cse168pc-04.cs.york.ac.uk>
2019-11-07 12:35:23 +0000

    7 segment done

--- common_utils/I2C7Seg.c
+++ common_utils/I2C7Seg.c
@@ -1,4 +1,3 @@
-
 #include <common_utils/I2C7Seg.h>
 #include <common_utils/TextOutput.h>
 #include <LPC17xx.h>
@@ -9,27 +8,26 @@
 
 
 
-uint8_t patterns[10] = {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x67};
-int current_digits[4];
+//Bit patterns representing each number, index = number, eg patterns[3] = bit pattern for 3
+uint8_t patterns[17] = {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x67,0x77,0x7C,0x39,0x5E,0x79,0x71,0x49};
+//Brightness settings 0x17 being dimmest, 0x77 being brightest
 uint8_t brightness_settings[7] = {0x17,0x27,0x37,0x47,0x57,0x67,0x77};
-//Turns off all 7seg displays
+
+//Stores the current digits being displayed, 99 is effective NULL
+int current_digits[4] = {99,99,99,99};
 
 
-void display_7seg(uint8_t* data)
+void display_7seg(uint8_t* data, int datalength)
 {
-    Status sent = i2c_send_data(0x38,data,sizeof(data));
-
+    Status sent = i2c_send_data(0x38,data,datalength);
 }
 
 
 void init_7seg(void)
 {   
     uint8_t data[6] = {0x00,0xF7,0x00,0x00,0x00,0x00};
-    display_7seg(data);
+    display_7seg(data,sizeof(data));
 }
-
-
-
 
 
 int get_bit_pattern(int number)
@@ -40,11 +38,11 @@ int get_bit_pattern(int number)
 
 
 void change_digit(int digit, int val)
-{   
+{  
     uint8_t bit_pattern = get_bit_pattern(val);
     uint8_t data[2] = {digit,bit_pattern} ;
     current_digits[digit] = val;
-    display_7seg(data);
+    display_7seg(data,sizeof(data));
 
 }
 
@@ -52,7 +50,7 @@ void change_digit(int digit, int val)
 void seven_seg_off (void)
 {
     uint8_t data[6] = {0x00,0x00,0x00,0x00,0x00,0x00};
-    display_7seg(data);
+    display_7seg(data,sizeof(data));
 
 }
 
@@ -68,23 +66,23 @@ void set_decimal_point(int state)
 {
     int new_pattern = get_bit_pattern(current_digits[2]);
 
-    if (state = 1){
+    if (state == 1){
         new_pattern = (new_pattern | 0x80);
+        
     }
-
     uint8_t data[2] = {0x3,new_pattern};
-    display_7seg(data);
+    display_7seg(data,sizeof(data));
 }
 
 
 void set_top_dot(int state)
 {
     int new_pattern = (get_bit_pattern(current_digits[1]));
-    if (state = 1){
+    if (state == 1){
         new_pattern = (new_pattern | 0x80);
     }
     uint8_t data[2] = {0x2,new_pattern};
-    display_7seg(data);
+    display_7seg(data,sizeof(data));
 }
 
 
@@ -92,24 +90,31 @@ void brightness_7seg(int setting)
 {
     uint8_t brightness = (brightness_settings[setting-1]);
     uint8_t data[2] = {0x00, brightness};
-    display_7seg(data);
+    display_7seg(data,sizeof(data));
 
 }
 
 
+void display_hex_7seg(int digit1, int digit2 )
+{
+    set_values(0,0, digit1, digit2,0,0);
+    
+    change_digit(2,sizeof(patterns)-1);
+}
 
-void set_values(int digit_one, int digit_two, int digit_three, int digit_four, int colon_state, int decimal_state, int topdot_state)
+
+void set_values(int digit_one, int digit_two, int digit_three, int digit_four, int decimal_state, int topdot_state)
 {
     int new_digits[4] = {digit_one, digit_two, digit_three, digit_four};
     int index = 1;
     for (index ; index<5 ; index++)
     {
-        if (new_digits[index] != current_digits[index]){
+        if (new_digits[index-1] != current_digits[index-1]){
             current_digits[index-1] = new_digits[index-1];
             change_digit(index, new_digits[index-1]);
         }
 
-    }
-
-
+    set_top_dot(topdot_state);
+    set_decimal_point(decimal_state);
+}
 }
--- common_utils/I2C7Seg.h
+++ common_utils/I2C7Seg.h
@@ -1,24 +1,32 @@
 #ifndef EMPR_I2C7SEG_H_
 #define EMPR_I2C7SEG_H_
+#include <stdint.h>
 
+//initialises the 7segment display
 void init_7seg(void);
 
-void display_7seg(uint8_t* data);
-
-int get_bit_pattern(int number);
-
+//Changes a specified digit 1-4 to a specified value 0x00 - 0x0F
 void change_digit(int digit, int val);
 
+//Turns off 7seg display
 void seven_seg_off(void);
 
+//Controls the colon, 1 = one, 0 = off
 void set_colon(int state);
 
+//Controls top dot, functions same as set_colon
 void set_top_dot(int state);
 
+//Controls the bottom dot, ^^
 void set_decimal_point(int state);
 
-void set_values(int digit_one, int digit_two, int digit_three, int digit_four, int colon_state, int decimal_state, int topdot_state);
+//One command to controll everthing at once, change all digits and control dots.
+void set_values(int digit_one, int digit_two, int digit_three, int digit_four, int decimal_state, int topdot_state);
 
+//changes brightness of 7seg dislay, setting range from 1-7
 void brightness_7seg(int setting);
+
+//displays 2 digit hex specified by digit 1 and digit 2
+void display_hex_7seg(int digit1, int digit2);
 
 #endif // EMPR_I2C7SEG_H_
