74f2d4e8696a6dc9d6bcb68cfcfe8b709c233dd2
lbowes <lewisbowes0@gmail.com>
2019-11-08 10:20:20 +0000

    Completed I2C receiving work
    This commit also contains work towards getting the keypad to function,
    because branches were not switched.

--- common_utils/Constants.h
+++ common_utils/Constants.h
@@ -7,22 +7,30 @@
 #define EMPR_OUTPUT       1
 
 // Keys
-#define EMPR_KEY_0        0b11101110
-#define EMPR_KEY_1        0b11101101
-#define EMPR_KEY_2        0b11101011
-#define EMPR_KEY_3        0b11100111
-#define EMPR_KEY_4        0b11011110
-#define EMPR_KEY_5        0b11011101
+// Top row
+#define EMPR_KEY_1        0b01110111
+#define EMPR_KEY_2        0b10110111
+#define EMPR_KEY_3        0b11010111
+#define EMPR_KEY_A        0b11100111
+
+// Upper middle row
+#define EMPR_KEY_4        0b01111011
+#define EMPR_KEY_5        0b10111011
 #define EMPR_KEY_6        0b11011011
-#define EMPR_KEY_7        0b11010111
-#define EMPR_KEY_8        0b10111110
-#define EMPR_KEY_9        0b10111101
-#define EMPR_KEY_A        0b10111011
-#define EMPR_KEY_B        0b10110111
-#define EMPR_KEY_C        0b01111110
-#define EMPR_KEY_D        0b01111101
-#define EMPR_KEY_ASTERISK 0b01111011
-#define EMPR_KEY_HASH     0b01110111
+#define EMPR_KEY_B        0b11101011
+
+// Lower middle row
+#define EMPR_KEY_7        0b01111101
+#define EMPR_KEY_8        0b10111101
+#define EMPR_KEY_9        0b11011101
+#define EMPR_KEY_C        0b11101101
+
+// Bottom row
+#define EMPR_KEY_0        0b01111110
+#define EMPR_KEY_D        0b10111110
+#define EMPR_KEY_ASTERISK 0b11011110
+#define EMPR_KEY_HASH     0b11101110
+
 #define EMPR_KEYPAD_ROWS  4
 #define EMPR_KEYPAD_COLS  EMPR_KEYPAD_ROWS
 
--- common_utils/Keypad.c
+++ common_utils/Keypad.c
@@ -5,6 +5,10 @@
 #include <common_utils/TextOutput.h>
 
 
+//temp
+#include <stdio.h>
+//
+
 static uint8_t keyDownStateByte[1];
 
 
@@ -16,35 +20,47 @@ void Keypad_init(void) {
 
 bool Keypad_isKeyDown(uint8_t key) {
     // Set all bits of the keypad state byte to 1's (pad is active LOW)
-    uint8_t allBitsHigh[1] = { 0xFF };
+    // Set the keyboard register to 00001111
+    uint8_t allBitsHigh[1] = { 0x0F };
     if(!i2c_send_data(KEYPAD_I2C_ADDRESS, allBitsHigh, 1)) {
         TextOutput_println("ERROR receiving keypad state byte.");
     }
 
     // Update the contents of the state byte
-    keyDownStateByte[0] = 0xFF;
     if(!i2c_receiveDataFrom(KEYPAD_I2C_ADDRESS, &keyDownStateByte[0], 1)) {
         TextOutput_println("ERROR receiving keypad state byte.");
     }
+    keyDownStateByte[0] |= 0xF0;
+
+    // keyDownStateByte[0] is either
+    // 1111 1111 = empty
+    // 1111 1110 = bottom row
+    // 1111 1101 = lower mid
+    // 1111 1011 = upper mid
+    // 1111 0111 = top row
+
+    // char msg[3];
+    // sprintf(msg, "%d", keyDownStateByte[0]);
+    // TextOutput_print(msg);
+
+    // 0111 1111
+    uint8_t keyColumn = key | 0x0F;
+    char msg[3];
+    sprintf(msg, "%d", keyColumn);
+    TextOutput_print(msg);
 
 
-    // TODO: Fix logic in here
-    
-    //uint8_t val = ~(1 << 5);
-    // 0010 0000
-    // 11011111
+    return (keyDownStateByte[0] & keyColumn) == key;
 
-    //char msg[128];
-    //sprintf(msg, "Val: %d", val);
-    //TextOutput_print(msg);
+    // uint8_t i = 0;
+    // for(i = 0; i < EMPR_KEYPAD_COLS; i++) {
+    //     uint8_t colIdx = ~(1 << (4 + i)) | 0x0F;
+    //     uint8_t selectedKey = keyDownStateByte[0] & colIdx;
 
-    uint8_t i = 0;
-    for(i = 0; i < EMPR_KEYPAD_ROWS; i++) {
-        uint8_t rowIdx = ~(1 << (4 + i));
-        uint8_t selectedKey = keyDownStateByte[0] & rowIdx;
-        if(selectedKey == key)
-            return true;
-    }
+    //     if(selectedKey == key)
+    //         return true;
+    // }
 
-    return false;
+    //return false;
 }
+
