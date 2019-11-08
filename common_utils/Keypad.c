2f824ac2cb5480aeef5e20e78acf8edd3099de00
lbowes <lewisbowes0@gmail.com>
2019-11-08 11:16:12 +0000

    Basic keypad functionality complete
    This commit completes the implementation of `Keypad_isKeyDown()`
    which returns true or false for a given key defined in `Constants.h`.
    
    A potential improvement would be storing and updating the state of
    the keypad in one go, so that we could check whether multiple keys
    were down at the same time.

--- common_utils/Keypad.c
+++ common_utils/Keypad.c
@@ -2,65 +2,27 @@
 
 #include <common_utils/I2C.h>
 #include <common_utils/Constants.h>
-#include <common_utils/TextOutput.h>
-
-
-//temp
 #include <stdio.h>
-//
-
-static uint8_t keyDownStateByte[1];
 
 
 void Keypad_init(void) {
-    TextOutput_init();
     i2c_init();
 }
 
 
 bool Keypad_isKeyDown(uint8_t key) {
-    // Set all bits of the keypad state byte to 1's (pad is active LOW)
-    // Set the keyboard register to 00001111
-    uint8_t allBitsHigh[1] = { 0x0F };
-    if(!i2c_send_data(KEYPAD_I2C_ADDRESS, allBitsHigh, 1)) {
-        TextOutput_println("ERROR receiving keypad state byte.");
+    uint8_t col = 0;
+    for(col = 0; col < EMPR_KEYPAD_COLS; col++) {
+        uint8_t colMask[1] = { ~(1 << (4 + col)) | 0x0F };
+        i2c_send_data(KEYPAD_I2C_ADDRESS, colMask, 1);
+
+        uint8_t keyDownStateByte[1];
+        i2c_receiveDataFrom(KEYPAD_I2C_ADDRESS, &keyDownStateByte[0], 1);
+
+        if(key == keyDownStateByte[0])
+            return true;
     }
 
-    // Update the contents of the state byte
-    if(!i2c_receiveDataFrom(KEYPAD_I2C_ADDRESS, &keyDownStateByte[0], 1)) {
-        TextOutput_println("ERROR receiving keypad state byte.");
-    }
-    keyDownStateByte[0] |= 0xF0;
-
-    // keyDownStateByte[0] is either
-    // 1111 1111 = empty
-    // 1111 1110 = bottom row
-    // 1111 1101 = lower mid
-    // 1111 1011 = upper mid
-    // 1111 0111 = top row
-
-    // char msg[3];
-    // sprintf(msg, "%d", keyDownStateByte[0]);
-    // TextOutput_print(msg);
-
-    // 0111 1111
-    uint8_t keyColumn = key | 0x0F;
-    char msg[3];
-    sprintf(msg, "%d", keyColumn);
-    TextOutput_print(msg);
-
-
-    return (keyDownStateByte[0] & keyColumn) == key;
-
-    // uint8_t i = 0;
-    // for(i = 0; i < EMPR_KEYPAD_COLS; i++) {
-    //     uint8_t colIdx = ~(1 << (4 + i)) | 0x0F;
-    //     uint8_t selectedKey = keyDownStateByte[0] & colIdx;
-
-    //     if(selectedKey == key)
-    //         return true;
-    // }
-
-    //return false;
+    return false;
 }
 
