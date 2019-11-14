872a429697da8290799ecffbfb434aeecb6bd2db
lbowes <lewisbowes0@gmail.com>
2019-11-14 10:31:16 +0000

    Allow printing `int`s passed into `TextOutput_printInteger()`

--- playground/src/main.c
+++ playground/src/main.c
@@ -4,28 +4,17 @@
 // • Display the 4-bit (0-15) number on the LEDs with each number displayed for about 1 second under the control of timer-based interrupts.
 // • Simultaneously display the 0-15 count on the terminal screen in decimal, hexadecimal and binary.
 // • Print: “Finished count” on the terminal screen.
-#include <common_utils/I2C.h>
-#include <common_utils/LEDs.h>
+
 #include <common_utils/TextOutput.h>
-#include <common_utils/Keypad.h>
-#include <common_utils/LCDDisplay.h>
-#include <common_utils/RealTimeClock.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <common_utils/Interrupts.h>
+
+
+Interrupts_handleAll() { }
 
 
 int main(void) {
-
-    // Init everything
-    i2c_init();
     TextOutput_init();
-    LEDs_init();
-    keypad_init();
-    LCDDisplay_init();
-    Real_Time_Clock_init();
-    // End init
-   LCDDisplay_print("Initlising ...",LINE_1);
-   
-    return 1;
+    TextOutput_printInteger(2134345);
 
+    return 0;
 }
