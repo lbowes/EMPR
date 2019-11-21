a8a68ccf8aaaefcf042af875fa7575e8071e08b2
lbowes <lewisbowes0@gmail.com>
2019-11-21 09:59:05 +0000

    Minor change to main.c in `playground`
    This commit should not be required...

--- playground/src/main.c
+++ playground/src/main.c
@@ -1,10 +1,3 @@
-// MP1 demonstration: This demonstration requires a program that will
-// execute the following:
-// • Print: “Starting count” on the terminal screen.
-// • Display the 4-bit (0-15) number on the LEDs with each number displayed for about 1 second under the control of timer-based interrupts.
-// • Simultaneously display the 0-15 count on the terminal screen in decimal, hexadecimal and binary.
-// • Print: “Finished count” on the terminal screen.
-
 #include <common_utils/TextOutput.h>
 #include <common_utils/Interrupts.h>
 
