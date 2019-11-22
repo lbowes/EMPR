1be40c506c87fd60d477eb3aa776675143ff2a8e
Asim Poptani <asim@poptani.com>
2019-11-22 15:19:03 +0000

    header

--- common_utils/Interrupts.c
+++ common_utils/Interrupts.c
@@ -2,21 +2,20 @@
 #include "lpc17xx_libcfg_default.h"
 #include "lpc17xx_systick.h"
 
-
 static uint32_t elapsedMs = 0;
 // Create 100 callbackes
 static void (*callbacks[100])();
-static uint8_t head =0;
+static uint8_t head = 0;
 static void (*interruptHandler)();
 
 void Do_Nothing(void)
 {
-
 }
 
-void SysTick_Handler(void) {
+void SysTick_Handler(void)
+{
     int handler;
-    for (handler=0; handler<head; handler++)
+    for (handler = 0; handler < head; handler++)
     {
         callbacks[handler]();
     }
@@ -24,34 +23,44 @@ void SysTick_Handler(void) {
     elapsedMs++;
 }
 
-int  Interrupt_add(void (*callback)())
+int Interrupt_add(void (*callback)())
 {
     head++;
-    callbacks[head]=callback;
+    callbacks[head] = callback;
     return head;
 }
 
-void Interrupt_remove(int callback)
-{void
-    callbacks[callback]=Do_Nothing;
+int Interrupt_add_at_head(int loc, void (*callback)())
+{
+    if (head >= loc && loc >= 0)
+    {
+        callbacks[loc] = callback;
+        return loc;
+    }
+    return -1
 }
 
+void Interrupt_remove(int callback)
+{
+    callbacks[callback] = Do_Nothing;
+}
 
-void Interrupts_start(void (*callback)()) {
-    callbacks[head]=callback;
+void Interrupts_start(void (*callback)())
+{
+    callbacks[head] = callback;
 
     SYSTICK_InternalInit(1);
     SYSTICK_IntCmd(ENABLE);
     SYSTICK_Cmd(ENABLE);
 }
 
-
-void Interrupts_stop(void) {
+void Interrupts_stop(void)
+{
     SYSTICK_IntCmd(DISABLE);
     SYSTICK_Cmd(DISABLE);
 }
 
-
-bool Interrupts_every(uint32_t ms) {
+bool Interrupts_every(uint32_t ms)
+{
     return elapsedMs % ms == 0;
 }
--- common_utils/Interrupts.h
+++ common_utils/Interrupts.h
@@ -9,8 +9,11 @@
 
 
 void Interrupts_start(void (*callback)());
+int Interrupt_add(void (*callback)());
 void Interrupts_stop(void);
 bool Interrupts_every(uint32_t ms);
+void Do_Nothing(void);
+void Interrupt_remove(int callback);
 
 // User defined
 void Interrupts_handleAll(void);
