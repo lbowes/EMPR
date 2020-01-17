7801221a3e5d5131acab9d6efc8221632fc6dd86
lbowes <lewisbowes0@gmail.com>
2020-01-17 12:56:44 +0000

    Made RGBC a typedef struct and moved it into the  file

--- common_utils/RGBC.h
+++ common_utils/RGBC.h
@@ -1,9 +1,20 @@
 #ifndef EMPR_RGBC_H_
 #define EMPR_RGBC_H_
 
+#include <stdint.h>
+
+//Struct
+typedef struct
+{
+    uint16_t r;
+    uint16_t g;
+    uint16_t b;
+    uint16_t c;
+} RGBC;
+
 //initialises the 7segment display
 void RGBC_init(void);
 
 // Scan 
-struct RGBC RGBC_SCAN(void);
+RGBC RGBC_SCAN(void);
 #endif // EMPR_I2C7SEG_H_
