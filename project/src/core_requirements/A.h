ced64942863906e43c69adbf24724e65a93d6fcd
lbowes <lewisbowes0@gmail.com>
2020-01-29 11:17:54 +0000

    Add core requirements implementation framework
    Also, this commit slightly restructures the project by separating
    requirements implementation from scanner library code.

---
+++ project/src/core_requirements/A.h
@@ -0,0 +1,8 @@
+#ifndef EMPR_REQ_A_H_
+#define EMPR_REQ_A_H_
+
+void A1();
+void A2();
+void A3();
+
+#endif // EMPR_REQ_A_H_
---
+++ project/src/core_requirements/C.h
@@ -0,0 +1,6 @@
+#ifndef EMPR_REQ_C_H_
+#define EMPR_REQ_C_H_
+
+void C1();
+
+#endif // EMPR_REQ_C_H_
