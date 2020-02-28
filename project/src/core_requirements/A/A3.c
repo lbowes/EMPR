58b16714cd0153838383fe8d13f5dcdfde4d3d53
DJ Ramsden <djr543@cse168pc-33.cs.york.ac.uk>
2020-02-28 16:51:38 +0000

    comment everything out

--- project/src/core_requirements/A/A3.c
+++ project/src/core_requirements/A/A3.c
@@ -1,116 +1,116 @@
-#include "A3.h"
+// #include "A3.h"
 
-#include <mbed/Constants.h>
-#include "scanner/Motion.h"
+// #include <mbed/Constants.h>
+// #include "scanner/Motion.h"
 
-#include "scanner/PcSender.h"
-#include <mbed/LCDDisplay.h>
-#include <mbed/Keypad.h>
-
-
-#include "stdbool.h"
-#include "stdio.h"
-#include "scanner/Vector3D.h"
-#include "lpc17xx_pinsel.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
-#include "mbed/7Seg.h"
-#include "mbed/I2C.h"
-#include "mbed/LCDDisplay.h"
+// #include "scanner/PcSender.h"
+// #include <mbed/LCDDisplay.h>
+// #include <mbed/Keypad.h>
 
 
-
-/*
- * 'Manual Move Mode,' : the user should be able to use the keypad or another
- * input device to move all three axes back and forth, whilst the position of each axis
- * is displayed on the LCD panel.
- * • The user can move the x-y position using suitable inputs
- * • The Z-axis can be controlled to at least two positions (Up/Down)
- * • Response times should be fast enough for reasonable user interaction.
-*/
+// #include "stdbool.h"
+// #include "stdio.h"
+// #include "scanner/Vector3D.h"
+// #include "lpc17xx_pinsel.h"
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
+// #include <math.h>
+// #include "mbed/7Seg.h"
+// #include "mbed/I2C.h"
+// #include "mbed/LCDDisplay.h"
 
 
 
-char buffer[20];
+// /*
+//  * 'Manual Move Mode,' : the user should be able to use the keypad or another
+//  * input device to move all three axes back and forth, whilst the position of each axis
+//  * is displayed on the LCD panel.
+//  * • The user can move the x-y position using suitable inputs
+//  * • The Z-axis can be controlled to at least two positions (Up/Down)
+//  * • Response times should be fast enough for reasonable user interaction.
+// */
 
 
 
-int scale = 5;
-
-Vector3D location ;
+// char buffer[20];
 
 
-void move_x1(int change){
-    Motion_moveBy(change, 0, 0);
-    location = Motion_getCurrentPos() ; 
-    sprintf(buffer,"%d   %d", location.x, location.y);
-    LCDDisplay_print(buffer, 0);
-}
+
+// int scale = 5;
+
+// Vector3D location ;
 
 
+// void move_x1(int change){
+//     Motion_moveBy(change, 0, 0);
+//     location = Motion_getCurrentPos() ; 
+//     sprintf(buffer,"%d   %d", location.x, location.y);
+//     LCDDisplay_print(buffer, 0);
+// }
 
 
 
 
-void move_y1(int change){
-    Motion_moveBy(0, change, 0);
-    location = Motion_getCurrentPos() ; 
-    sprintf(buffer,"%d   %d", location.x, location.y);
-    LCDDisplay_print(buffer, 0);
+
+
+// void move_y1(int change){
+//     Motion_moveBy(0, change, 0);
+//     location = Motion_getCurrentPos() ; 
+//     sprintf(buffer,"%d   %d", location.x, location.y);
+//     LCDDisplay_print(buffer, 0);
    
-}
+// }
 
 
 
 
 
-void move_z1(int change){
-    Motion_moveBy(0, 0, change);
-    location = Motion_getCurrentPos() ; 
-    sprintf(buffer,"%d", location.z);
-    LCDDisplay_print(buffer, 1);
-}
+// void move_z1(int change){
+//     Motion_moveBy(0, 0, change);
+//     location = Motion_getCurrentPos() ; 
+//     sprintf(buffer,"%d", location.z);
+//     LCDDisplay_print(buffer, 1);
+// }
 
 
 
 
 
 
-void A3() {
-    i2c_init();
-    Motion_init();
-    Motion_home();
-    Keypad_init();
-    int neut_count  = 0;
-    while (1){
-        if (Keypad_isKeyDown(EMPR_KEY_1)){
-            move_x(scale);
-        }
-        if (Keypad_isKeyDown(EMPR_KEY_2)){
-            move_x(-scale);
-        } 
-        if (Keypad_isKeyDown(EMPR_KEY_4)){
-            move_y(scale);
-        }
-        if (Keypad_isKeyDown(EMPR_KEY_5)){
-            move_y(-scale);
-        } 
-        if (Keypad_isKeyDown(EMPR_KEY_7)){
-            move_z(5 * scale);
-        }
-        if (Keypad_isKeyDown(EMPR_KEY_8)){
-            move_z(-5 * scale);
-        } 
+// void A3() {
+//     i2c_init();
+//     Motion_init();
+//     Motion_home();
+//     Keypad_init();
+//     int neut_count  = 0;
+//     while (1){
+//         if (Keypad_isKeyDown(EMPR_KEY_1)){
+//             move_x(scale);
+//         }
+//         if (Keypad_isKeyDown(EMPR_KEY_2)){
+//             move_x(-scale);
+//         } 
+//         if (Keypad_isKeyDown(EMPR_KEY_4)){
+//             move_y(scale);
+//         }
+//         if (Keypad_isKeyDown(EMPR_KEY_5)){
+//             move_y(-scale);
+//         } 
+//         if (Keypad_isKeyDown(EMPR_KEY_7)){
+//             move_z(5 * scale);
+//         }
+//         if (Keypad_isKeyDown(EMPR_KEY_8)){
+//             move_z(-5 * scale);
+//         } 
 
-        if (neut_count == 1500) {
-            Motion_neutraliseAllAxes();
-            neut_count = 0;
-        }
+//         if (neut_count == 1500) {
+//             Motion_neutraliseAllAxes();
+//             neut_count = 0;
+//         }
         
-        neut_count ++;
-    }
-}
+//         neut_count ++;
+//     }
+// }
 
 
--- project/src/core_requirements/D/D2.c
+++ project/src/core_requirements/D/D2.c
@@ -1,68 +1,68 @@
-#include "D2.h"
+// #include "D2.h"
 
 
-#include <mbed/Constants.h>
-#include "scanner/Motion.h"
-#include "scanner/Colour.h"
-#include "scanner/PcSender.h"
+// #include <mbed/Constants.h>
+// #include "scanner/Motion.h"
+// #include "scanner/Colour.h"
+// #include "scanner/PcSender.h"
 
-#include "stdbool.h"
-#include "stdio.h"
-#include "lpc17xx_uart.h"
-#include "scanner/Vector3D.h"
-#include "lpc17xx_pinsel.h"
-#include "scanner/ColourSensor.h"
-
-
-
-#include "mbed/Serial.h"
-#include "mbed/7Seg.h"
-#include "mbed/I2C.h"
-#include "mbed/LCDDisplay.h"
-#include "mbed/TextOutput.h"
-
-#include "scanner/UART_movement.h"
+// #include "stdbool.h"
+// #include "stdio.h"
+// #include "lpc17xx_uart.h"
+// #include "scanner/Vector3D.h"
+// #include "lpc17xx_pinsel.h"
+// #include "scanner/ColourSensor.h"
 
 
 
+// #include "mbed/Serial.h"
+// #include "mbed/7Seg.h"
+// #include "mbed/I2C.h"
+// #include "mbed/LCDDisplay.h"
+// #include "mbed/TextOutput.h"
+
+// #include "scanner/UART_movement.h"
 
 
 
-uint8_t command[2];
-
-Vector3D location ;
-
-Colour scan_vals;
 
 
-void move(int x, int y){
-    Motion_moveTo(x,y,0);
 
-}
+// uint8_t command[2];
+
+// Vector3D location ;
+
+// Colour scan_vals;
 
 
-void D2() {
-    ColourSensor_init();
-    Motion_init();
-    i2c_init();
-    serial_init();
-    UART_IntConfig((LPC_UART_TypeDef *) LPC_UART0, UART_INTCFG_RBR, ENABLE);
-    NVIC_EnableIRQ(UART0_IRQn);
-    Motion_home();
-    Motion_neutraliseAllAxes();
-}
+// void move(int x, int y){
+//     Motion_moveTo(x,y,0);
 
-//DO NOT UNCOMMENT, cannot define multiple UART IRQ handlers, multiple being used, will merge them together 
-
-// void UART0_IRQHandler(void){
-//     NVIC_DisableIRQ(UART0_IRQn);
-//     memset(command, 0, sizeof(command));
-//     serial_read(&command , sizeof(command));
-
-//     move(command[0],command[1]);
-
-//     scan_vals = ColourSensor_read();
-//     PCSender_sendRGBAndPos (0,0,0,scan_vals.r, scan_vals.g, scan_vals.b, scan_vals.clear);
-//     Motion_neutraliseAllAxes();
-//     NVIC_EnableIRQ(UART0_IRQn);
 // }
+
+
+// void D2() {
+//     ColourSensor_init();
+//     Motion_init();
+//     i2c_init();
+//     serial_init();
+//     UART_IntConfig((LPC_UART_TypeDef *) LPC_UART0, UART_INTCFG_RBR, ENABLE);
+//     NVIC_EnableIRQ(UART0_IRQn);
+//     Motion_home();
+//     Motion_neutraliseAllAxes();
+// }
+
+// //DO NOT UNCOMMENT, cannot define multiple UART IRQ handlers, multiple being used, will merge them together 
+
+// // void UART0_IRQHandler(void){
+// //     NVIC_DisableIRQ(UART0_IRQn);
+// //     memset(command, 0, sizeof(command));
+// //     serial_read(&command , sizeof(command));
+
+// //     move(command[0],command[1]);
+
+// //     scan_vals = ColourSensor_read();
+// //     PCSender_sendRGBAndPos (0,0,0,scan_vals.r, scan_vals.g, scan_vals.b, scan_vals.clear);
+// //     Motion_neutraliseAllAxes();
+// //     NVIC_EnableIRQ(UART0_IRQn);
+// // }
